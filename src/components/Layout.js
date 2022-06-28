f7478f423686e7d5420beda653145a09d417c9f0
YeonJun Cho <micayel1219@gmail.com>
2022-06-28 15:40:02 +0900

    blog page add

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -39,6 +39,11 @@ function Layout({ pageTitle, children }) {
               About
             </Link>
           </li>
+          <li className={navLinkItem}>
+            <Link className={navLinkText} to="/blog">
+              blog
+            </Link>
+          </li>
         </ul>
       </nav>
 
